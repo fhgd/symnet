e8fe1b4dafadf4d6742f1a79df1780321af69855
Friedrich Hagedorn <fhgd@users.noreply.github.com>
2014-09-01 16:55:48 +0200

    FVF: remove all the non mna printed lines

--- flipped_voltage_follower.py
+++ flipped_voltage_follower.py
@@ -1,4 +1,3 @@
-from sympycore import Matrix
 from symnet import *
 
 """
@@ -34,33 +33,13 @@ RS      OUT 0
 RC2     OUT 0
 """
 g, ctrl_src = parse_netlist(netlist)
-
-tree = g.tree(['Rds2', 'Vin', 'RC2'])
-eqs, x = cut_analysis(g, ctrl_src, tree)
 eqs, x, tree = mna(g, ctrl_src)
 
 A, b = create_matrices(eqs, x)
 eqstr = pprint_linear(A, x, b)
-
-treebrns = tree.branches()
-cobrns = g.branches() - treebrns
-
-print '* Baum', treebrns, ',  Ctrls', ctrl_src, ',  Cobaum', cobrns
-print
 print eqstr
 print
 
 print 'Mathematica:'
 print pprint_mathematica(eqs, x)
-print
-
-num, denom = solve_linear(Matrix(A), x, Matrix(b), 'VOUT')
-num = num.subs('Vin', 1) * 'Vin'  # factor out constant voltage source
-subs = {'G_RC1':'s*C1', 'G_RC2':'s*C2'}
-
-print 'Vout := num/denom := V_RC2'
-print
-print 'num   =', num.subs(subs)
-print
-print 'denom =', denom.subs(subs)
 
