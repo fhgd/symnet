464d2efabfc72f2b8b23f377379cfe15b7a8ef6b
Friedrich Hagedorn <fhgd@users.noreply.github.com>
2014-11-28 13:22:17 +0100

    opamp: implement Sallen?Key topology

--- opamp.py
+++ opamp.py
@@ -1,22 +1,21 @@
+import symnet
+reload(symnet)
 from symnet import *
 
 g, ctrl_src = parse_netlist("""
-    Vi  P   0
-    U1  P   N
-    N1  A   0
-    R1  A   N
-    R2  N   0
+    NR  A   0
+    UL  P   A
+    R4  P   0
+    R2  X   P
+    R3  X   A
+    R1  I   X
+    Vi  I   0
 """)
 
 
-#~ tree = g.tree('Vi U1 N1'.split())   # best for cut analysis
-tree = g.tree('Vi R2 R1'.split())   # best for loop analysis
+#~ tree = g.tree('Vi NR R3 UL'.split())   # best for cut analysis
+tree = g.tree('Vi NR R1 R2'.split())   # best for loop analysis
 
-#~ tree = g.tree('Vi R2 R1'.split())
-#~ tree = g.tree('Vi R2 N1'.split())
-#~ tree = g.tree('Vi U1 R1'.split())
-#~ tree = g.tree('U1 R2 R1'.split())
-#~ tree = g.tree('U1 N1 R2'.split())
 
 #~ eqs, x = cut_analysis(g, {}, tree)
 #~ eqs, x, tree = mna(g, ctrl_src)
@@ -27,7 +26,7 @@ eqstr = pprint_linear(A, x, b)
 print eqstr
 print
 
-num, denom = solve_linear(Matrix(A), x, Matrix(b), 'V_N1')
+num, denom = solve_linear(Matrix(A), x, Matrix(b), 'V_NR')
 num = num.subs('Vi', 1) * 'Vi'  # factor out constant voltage source
 
 num = str(num).replace('G_R', 'G')
@@ -36,3 +35,4 @@ print num
 print '-' * max(len(str(num)), len(str(denom)))
 print denom
 print
+
